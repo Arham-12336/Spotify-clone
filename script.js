bceb89739179749c5d4823ff4830851da796232d
Muhammad Arham <67412607+Arham-12336@users.noreply.github.com>
2022-12-31 00:40:12 +0500

    Updated list

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ let audioElement=new Audio('songs/1.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
 let gif=document.getElementById('gif');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
 let songs=[
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath:"song/1.mp3", coverpath: "cover/1.jpg"}
+    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+
 ]
+
+songItems.forEach((element,i) => {
+    element.getElementsByTagName("img")[0].src =songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+});
 
 //Handle Play 
 masterPlay.addEventListener('click',()=>{
@@ -34,7 +41,7 @@ masterPlay.addEventListener('click',()=>{
     }
 
 })
-
+ 
 
 
 
@@ -47,7 +54,7 @@ audioElement.addEventListener('timeupdate',()=>{
 })
 
 myProgressBar.addEventListener('change',()=>{
-    audioElement.currentTime=myProgressBar.value*audioElement.duration;
+    audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
 })
 //Listen to Events
 
